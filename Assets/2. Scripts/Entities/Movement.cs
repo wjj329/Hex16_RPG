4464f8cf19d310542b333b22c32ad4cef441749e
wjj329 <wjj2187@naver.com>
2024-02-15 10:14:42 +0900

    feat. 넉백 시스템 적용

--- Assets/2. Scripts/Entities/Movement.cs
+++ Assets/2. Scripts/Entities/Movement.cs
@@ -9,8 +9,9 @@ public class Movement : MonoBehaviour
     private CharacterStatsHandler _stats;
 
     private Vector2 _movementDirection = Vector2.zero; /// 현재 움직임 방향
-    
 
+    private Vector2 _knockback = Vector2.zero;
+    private float knockbackDuration = 0.0f;
 
     private void Awake()
     {
@@ -29,6 +30,11 @@ public class Movement : MonoBehaviour
     {
         ApplyMovment(_movementDirection);
         // 움직임 방향에 따라 물리 움직임 적용
+
+        if (knockbackDuration > 0.0f)
+        {
+            knockbackDuration -= Time.fixedDeltaTime;
+        }
     }
 
     private void Move(Vector2 direction)
@@ -37,10 +43,21 @@ public class Movement : MonoBehaviour
         // 입력된 움직임 방향 저장
     }
 
+    public void ApplyKnockback(Transform other, float power, float duration)
+    { // 넉백 적용
+        knockbackDuration = duration;
+        _knockback = -(other.position - transform.position).normalized * power;
+    } 
+
     private void ApplyMovment(Vector2 direction)
     {
         // 이동속도 설정
         direction = direction * _stats.CurrentStats.speed;
+
+        if (knockbackDuration > 0.0f)
+        {
+            direction += _knockback;
+        }
 
         _rigidbody.velocity = direction;
         // Rigidbody의 속도를 설정하여 움직임을 적용
