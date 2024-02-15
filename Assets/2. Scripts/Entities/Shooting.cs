591f130043f496251c852b00bd1671ddd8e89947
wjj329 <wjj2187@naver.com>
2024-02-15 10:02:24 +0900

    feat. 투사체 구현2

--- Assets/2. Scripts/Entities/Shooting.cs
+++ Assets/2. Scripts/Entities/Shooting.cs
@@ -7,9 +7,9 @@ public class Shooting : MonoBehaviour
 {
     private CharacterController _contoller; // CharacterController참조
 
-    [SerializeField] private Transform BulletSpawnPosition; // 탄환 스폰 위치 지정용 (유니티)
-    
-    public GameObject BulletPrefab; // 탄환 프리팹 지정용 (유니티)
+    [SerializeField] private Transform projectileSpawnPosition; // 탄환 스폰 위치 지정용 (유니티)
+
+    private ProjectileManager _projectileManager; // 프로젝타일 매니저 참조
 
     private Vector2 _aimDirection = Vector2.right; // 기본 조준 방향
 
@@ -20,23 +20,59 @@ public class Shooting : MonoBehaviour
 
     void Start()
     {
-        //_contoller.OnAttackEvent += OnShoot; // OnAttackEvent에 OnShoot 메소드 연결
-        _contoller.OnLookEvent += OnAim; // OnLookEvent 이벤트에 OnAim 메소드 연결
+        _projectileManager = ProjectileManager.instance;
+        _contoller.OnAttackEvent += OnShoot;
+        _contoller.OnLookEvent += OnAim;
     }
+
 
     private void OnAim(Vector2 newAimDirection)
     {
         _aimDirection = newAimDirection; // 조준 방향 업데이트
     }
 
-    private void OnShoot()
+    private void OnShoot(AttackSO attackSO)
     {
-        CreateProjectile(); // 탄환 생성 메서드 호출
+        // attackSO에서 RangedAttackData로 형 변환
+        RangedAttackData rangedAttackData = attackSO as RangedAttackData;
+
+        // 각 발사체 사이 각도
+        float projectilesAngleSpace = rangedAttackData.multipleProjectilesAngel;
+
+        // 샷 당 발사체의 수
+        int numberOfProjectilesPerShot = rangedAttackData.numberofProjectilesPerShot;
+
+        // 첫 발사체 각도 계산
+        float minAngle = -(numberOfProjectilesPerShot / 2f) * projectilesAngleSpace + 0.5f * rangedAttackData.multipleProjectilesAngel;
+
+
+        //설정 발사체 수만큼 발사체 생성
+        for (int i = 0; i < numberOfProjectilesPerShot; i++)
+        {
+            // 각 발사체 각도 계산
+            float angle = minAngle + projectilesAngleSpace * i;
+
+            // 발사체 각도 랜덤 분산
+            float randomSpread = Random.Range(-rangedAttackData.spread, rangedAttackData.spread);
+            angle += randomSpread;
+
+            CreateProjectile(rangedAttackData, angle); // 발사체 생성 메서드 호출
+        }
     }
 
-    private void CreateProjectile() // 탄환(프리팹) 생성
+    // ProjectileManager.ShootBullet 메서드 호출
+    // 위치와 방향 = 각도에 따라 조정
+    private void CreateProjectile(RangedAttackData rangedAttackData, float angle)
     {
-        Instantiate(BulletPrefab, BulletSpawnPosition.position, Quaternion.identity); // 프리팹을 기반으로 발사체를 생성합니다.
+        _projectileManager.ShootBullet(
+                projectileSpawnPosition.position,
+                RotateVector2(_aimDirection, angle),
+                rangedAttackData
+                );
+    }
+    private static Vector2 RotateVector2(Vector2 v, float degree)
+    {
+        return Quaternion.Euler(0, 0, degree) * v;
     }
 }
 
--- Assets/2. Scripts/Manager/ProjectileManager.cs
+++ Assets/2. Scripts/Manager/ProjectileManager.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class ProjectileManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private ParticleSystem _impactParticleSystem; //파티클 시스템
+
+    public static ProjectileManager instance;
+
+    [SerializeField] private GameObject testObj; // 투사체 프리팹 (유니티)
+
+    private void Awake()
     {
-        
+        instance = this;
     }
 
-    // Update is called once per frame
-    void Update()
+    void Start()
     {
-        
+
     }
+
+    public void ShootBullet(Vector2 startPostiion, Vector2 direction, RangedAttackData attackData)
+    {
+        // 투사체 인스턴스화
+        GameObject obj = Instantiate(testObj);
+
+        
+        obj.transform.position = startPostiion;
+
+        // 투사체 RangedAttackController 컴포넌트 초기화
+        RangedAttackController attackController = obj.GetComponent<RangedAttackController>();
+        attackController.InitializeAttack(direction, attackData, this);
+
+        obj.SetActive(true);
+    }
+
 }
